60804303673df9294ddf8818ec355ce4321c4e46
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2023-03-26 19:09:40 +0530

    Updated psql_init file, added temporary floor coordinates in eee.csv

--- psql_init.sh
+++ psql_init.sh
@@ -4,14 +4,18 @@ expot PGPASSWORD=""
 # General form
 # psql -U <user> -d <dbname> -c ""
 
-psql_USERNAME=""
-psql_DBNAME=""
+psql_USERNAME="cet_go"
+psql_DBNAME="cet_go_db"
 
-# create room_details table
-psql -U $psql_USERNAME -d $psql_DBNAME -c "CREATE TABLE room_details(id varchar, X varchar, Y varchar, Z varchar, val varchar, Fx varchar, Fy varchar);"
-
+# create user, database and give access rights
+sudo -u postgres createuser $psql_USERNAME
+sudo -u postgres createdb $psql_DBNAME
+sudo -u postgres psql -c "alter user $psql_USERNAME with encrypted password $PGPASSWORD;"
+sudo -u postgres psql -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO $psql_USERNAME";
+# sudo -u postgres psql -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO Read_Only_User;"
 # add data from csv
-for i in cse mca ce1
+for i in cse mca ce1 eee
 do
+	psql -U $psql_USERNAME -d $psql_DBNAME -c "CREATE TABLE $i(id varchar primary key, X varchar, Y varchar, Z varchar, val varchar, Fx varchar, Fy varchar);"
 	psql -U $psql_USERNAME -d $psql_DBNAME -c "\copy room_details(id, X, Y, Z, val, Fx, Fy) FROM '$i.csv' DELIMITER ',' CSV HEADER;"
 done
