2ef9108f48b57a48f6453cc81c7c56fb62164f31
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2023-06-22 15:36:11 +0530

    Updated python script to create neo4J database

--- neo4j_create_db.py
+++ neo4j_create_db.py
@@ -19,16 +19,33 @@ graphDB_Driver  = GraphDatabase.driver(uri_to_server, auth=(usr, pwd))
 # writing to a file for later use 
 
 with graphDB_Driver.session() as graphDB_Session:
-	graphDB_Session.run("MATCH(n) DETACH DELETE n")
+	print(" Removing already existing departments:")
+	res = graphDB_Session.run("MATCH(n) DETACH DELETE n")
+	print(" \t Nodes removed: ", res.consume().counters.nodes_deleted)
+	print(" \t Edges removed: ", res.consume().counters.relationships_deleted)
+	print(" Creating Departments in Neo4J: ")
 	for i in ['cse', 'ce1', 'mca', 'eee']:
 		r = ""
 		with open('{}.cypher'.format(i), 'r') as f:
 			r = f.read()
-			# print(r)
+			print(' \t Creating {} graph'.format(i.upper()))
 			# connecting with the neo4j driver		
 			graph_nodes = graphDB_Session.run(r)
+			print(" \t\t Nodes created: ", graph_nodes.consume().counters.nodes_created)
+			print(" \t\t Edges created: ", graph_nodes.consume().counters.relationships_created)
 			# checking if the nodes where returned correctly
-		res = graphDB_Session.run("MATCH(n) return n")
-		print(res)
+	res = graphDB_Session.run("MATCH(n) return n")
+	# print(res)
+	# records, summary, keys = graphDB_Driver.execute_query(
+	# 	"MATCH (n) RETURN n",
+	# 	database_="neo4j",
+	# )
+	# print("The query `{query}` returned {records_count} records in {time} ms.".format(
+	# 	query=summary.query, records_count=len(records),
+	# 	time=summary.result_available_after
+	# ))
+	print(" Nodes created: ", res.consume().counters.nodes_returned)
+	print(" Edges created: ", res.consume().counters.relationships_returned)
 
 graphDB_Driver.close()
+print(" Done !")
