eed36846772a17f6d82396653780616a8be53749
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2023-06-22 14:09:41 +0530

    Update ME images, start new EEE graph, add png folder
    Updated ME images to fit the floor,
    New EEE graph using new graph creation format
    PNG folder to store png versions of files

--- create_EEE_Ugraph.py
+++ create_EEE_Ugraph.py
@@ -3,30 +3,40 @@
 import math
 
 # credentials here to connect to database
-usr = ""
-uri_to_server = ""
-pwd = ""
-
+dept = "eee"
 
 # to find distance between nodes
 def eucledian_dist(x, y):
-	return(str(math.sqrt((y[0] - x[0])**2 + (y[1] - x[1])**2)))
+	return(str(math.sqrt((y[0] - x[0])**2 + (y[1] - x[1])**2 + (y[2] - x[2])**2)))
+
+def connect(str, nodes, start, end):
+	# print(nodes[start])
+	# print(nodes[end])
+	str = str + ' ({0})-[:neigh '.format(nodes[start][0]) +"{dist: " + eucledian_dist((nodes[start][1:4]), (nodes[end][1:4])) + "}]->(" + nodes[end][0] + '),\n'
+	return(str)
+
+def create_straight_line(l):
+	create_nodes_and_edges2 = ""
+	# print(l)
+	for i in range(len(l)-1):
+		create_nodes_and_edges2 = connect(create_nodes_and_edges2, l, i, (i+1)%len(l))
+		# connect(create_nodes_and_edges2, l, (i+1)%len(l), i)
+	return(create_nodes_and_edges2)
 
 # to create a circular floor path, useful in some cases
 def create_circular_relationship(l):
 	create_nodes_and_edges2 = ""
-	print(l)
+	# print(l)
 	for i in range(len(l)):
-		create_nodes_and_edges2 = create_nodes_and_edges2 + ' ({0})-[:neigh '.format(l[i][0]) +"{dist: " + eucledian_dist((l[i][1:3]), (l[(i+1)%len(l)][1:3])) + "}]->(" + l[(i+1)%len(l)][0] + '),\n'
-		# create_nodes_and_edges2 = create_nodes_and_edges2 + ' ({0})-[:neigh '.format(l[(i+1)%len(l)][0]) +"{dist: " + eucledian_dist((l[i][1:3]), (l[(i+1)%len(l)][1:3])) + "}]->(" + l[i][0] + '),'
+		create_nodes_and_edges2 = connect(create_nodes_and_edges2, l, i, (i+1)%len(l))
+		# connect(create_nodes_and_edges2, l, (i+1)%len(l), i)
 	return(create_nodes_and_edges2)
-
 
 # graphDB_Driver  = GraphDatabase.driver(uri_to_server, auth=(usr, pwd))
 
 
 # read nodes from csv file
-f = open("eee.csv", "r")
+f = open("{}.csv".format(dept), "r")
 nodes = []
 for i in f.readlines():
 	e = i.replace('"', '').replace('\t', '').split(',')
@@ -38,11 +48,14 @@ for i in f.readlines():
 		nodes = []
 		pass 
 
-# print(nodes)
-f.close()
-
 if(nodes == []):
 	raise SystemExit(0)
+nodes.insert(0, ["padding"])
+nodes.insert(0, ["padding"])
+
+# print(nodes)
+f.close()
+print(len(nodes))
 
 # Starting the create query
 create_nodes_and_edges = "CREATE"
@@ -57,18 +70,18 @@ for i in nodes[1:]:
 # 	create_nodes_and_edges = create_nodes_and_edges + " (" + i[0] + ":stairs { " +  'id: "{0}", desc: "{1}"'.format(i[0], i[4]) + "}),\n"
 
 # create relationships for floor -1
-create_nodes_and_edges1 = create_circular_relationship((nodes[1:7]))
+create_nodes_and_edges1 = create_straight_line((nodes[1:8]))
 
 # create relationships for floor 0
-create_nodes_and_edges2 = create_circular_relationship(nodes[7:15])
+create_nodes_and_edges2 = create_circular_relationship(nodes[8:16])
 # print(create_nodes_and_edges2)
 
 #create relationships for floor 1
-create_nodes_and_edges3 = create_circular_relationship(nodes[15:24])
+create_nodes_and_edges3 = create_circular_relationship(nodes[16:25])
 # print(create_nodes_and_edges3)
 
 # create relationships for floor 2
-create_nodes_and_edges4 = create_circular_relationship(nodes[24:33])
+create_nodes_and_edges4 = create_circular_relationship(nodes[25:33])
 # print(create_nodes_and_edges4)
 
 create_nodes_and_edges = create_nodes_and_edges + create_nodes_and_edges1 + create_nodes_and_edges2 + create_nodes_and_edges3 + create_nodes_and_edges4
@@ -76,48 +89,6 @@ create_nodes_and_edges = create_nodes_and_edges + create_nodes_and_edges1 + crea
 
 # create relationships for stairs
 create_nodes_and_edges5 = ""
-
-# # stair at level i connects levels i and i+1
-# a, b = (47, 9)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (47, 28)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (48, 16)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (48, 35)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (49, 28)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (50, 35)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (51, 2)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (51, 38)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (52, 38)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
-# a, b = (52, 42)
-# create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[a][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[b][0] + '),\n'
-# # create_nodes_and_edges5 = create_nodes_and_edges5 + ' ({0})-[:neigh '.format(nodes[b][0]) +"{dist: " + eucledian_dist((nodes[a][1:3]), (nodes[b][1:3])) + "}]->(" + nodes[a][0] + '),'
-
 
 # # Adding water purifiers
 
