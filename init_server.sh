7fcfad60ce510c479a36accdb0ff755155c1da4b
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2023-06-26 00:32:29 +0530

    Renamed CS files, added init_server.sh

---
+++ init_server.sh
@@ -0,0 +1,9 @@
+cd ..
+git clone https://github.com/divyaSrsh/CollegeNavigationSystem.git
+git clone https://github.com/HariSK20/CET_Go_server
+cd CET_Go_server
+python3 -m pip install -r requirements.txt
+gunicorn wsgi:app
+cd ../CollegeNavigationSystem
+npm install
+npm start
