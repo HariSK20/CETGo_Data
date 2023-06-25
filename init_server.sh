7620dd733fd3cfaff5222e0e001202ab27c2af84
HariSK20 <56635847+HariSK20@users.noreply.github.com>
2023-06-26 00:37:52 +0530

    Added command to install and update npm in init_server.sh

--- init_server.sh
+++ init_server.sh
@@ -5,5 +5,8 @@ cd CET_Go_server
 python3 -m pip install -r requirements.txt
 gunicorn wsgi:app
 cd ../CollegeNavigationSystem
+sudo apt install npm
+npm install -g npm@latest
 npm install
+npm run build
 npm start
